a78df7ed35d700ff5dd057b82f6dbd6c9962b61d
Elizeu Oliveira <homenzinhoptr@gmail.com>
2022-06-14 11:01:14 -0300

    add portainer docker instalation

--- script_dev.sh
+++ script_dev.sh
@@ -99,6 +99,7 @@ snap install postman
 
 
 ## Instalando Docker
+echo "Instalando Docker Engine"
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -126,3 +127,12 @@ newgrp docker
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 
 sudo chmod g+rwx "$HOME/.docker" -R
+
+## Instalando
+echo "Instalando containers . . ."
+
+echo "Portainer docker"
+docker volume create portainer_data
+
+docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
